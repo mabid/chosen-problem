031e86ad05c52a47adad49f5ee55a28661b9ff81
Abid Mahmood <mabid.mahmood@gmail.com>
2012-06-04 12:37:09 +0500

    problem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chson::Application.config.secret_token = '40bbac3936bd7b1e399c4d517ccc6b1cfc2d57de85761d6640e71558a6ad514e9cbd48e96c8f1eebc07e1caec6b925e6a6f235d3a27229e32590458a546e66d6'
